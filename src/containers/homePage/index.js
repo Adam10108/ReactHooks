a003158348c2aaf75adb3d22cb7fbd0f3290e818
Adam10108 <Adam_Atlas1998@hotmail.com>
2020-02-29 22:33:09 +0700

    add:create config-overrides.js

--- src/containers/homePage/index.js
+++ src/containers/homePage/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
+// @flow
+import { Row, Col } from 'antd'
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
 
 const HomePage = () => {
-  return <>HomePage</>
+  const [people, setPeople] = useState([
+    { firstName: 'Jame', lastName: 'Dean' },
+    { firstName: 'Rocky', lastName: 'Babo' }
+  ])
+
+  const [person, setPerson] = useState({})
+
+  return (
+    <div>
+      <div>Learn React Hooks</div>
+    </div>
+  )
 }
 export default HomePage
